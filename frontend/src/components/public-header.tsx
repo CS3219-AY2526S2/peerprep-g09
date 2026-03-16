5e678644cecb7978c636cc5a740ab1685222a7f8
waylonggggg <tanwaylon38@gmail.com>
2026-03-16 16:46:10 +0800

    Public header hover effect

--- frontend/src/components/public-header.tsx
+++ frontend/src/components/public-header.tsx
@@ -4,24 +4,24 @@ export function PublicHeader() {
     return (
         <div className="text-header-font px-6 py-6 sticky top-0 bg-main-beige max-w h-20 flex justify-between">
             <div className="flex gap-4">
-                <Link href="/" className="hover:opacity-70">
+                <Link href="/" className="transition duration-200 hover:opacity-70">
                     Peerprep
                 </Link>
-                <Link href="/" className="hover:opacity-70">
+                <Link href="/" className="transition duration-200 hover:opacity-70">
                     Features
                 </Link>
-                <Link href="/" className="hover:opacity-70">
+                <Link href="/" className="transition duration-200 hover:opacity-70">
                     Learn more
                 </Link>
-                <Link href="/" className="hover:opacity-70">
+                <Link href="/" className="transition duration-200 hover:opacity-70">
                     Dummy
                 </Link>
             </div>
             <div className="flex gap-4">
-                <Link href="/login" className="hover:opacity-70">
+                <Link href="/login" className="transition duration-200 hover:opacity-70">
                     Log in
                 </Link>
-                <Link href="/signup" className="hover:opacity-70">
+                <Link href="/signup" className="transition duration-200 hover:opacity-70">
                     Signup
                 </Link>
             </div>
