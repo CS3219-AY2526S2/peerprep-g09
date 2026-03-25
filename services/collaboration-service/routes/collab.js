0f67923d8910229900a5a3941ae7bdf027eb47e1
TasmiaH0508 <Tasmiahaque2004@gmail.com>
2026-03-25 23:14:24 +0800

    Change room id

--- services/collaboration-service/routes/collab.js
+++ services/collaboration-service/routes/collab.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+router.use(express.json())
+
 router.get('/', (req, res) => {
     res.send('Welcome to the Collab space');
 });
