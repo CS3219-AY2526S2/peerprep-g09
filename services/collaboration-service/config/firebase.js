b927327e9ad3b01c5e654eeeb23725749fefb8bb
TasmiaH0508 <Tasmiahaque2004@gmail.com>
2026-03-27 23:39:17 +0800

    Remove redundant code

--- services/collaboration-service/config/firebase.js
+++ services/collaboration-service/config/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBixf7jBkfbrB2fNJxTgS_vkg0vftqPWh8",
-  authDomain: "cs3219-886c1.firebaseapp.com",
-  projectId: "cs3219-886c1",
-  storageBucket: "cs3219-886c1.firebasestorage.app",
-  messagingSenderId: "24332783908",
-  appId: "1:24332783908:web:2dcf2dce99269e955fe3b1",
-  measurementId: "G-FM5GK60Q25"
+  // init here
 };
 
 // Initialize Firebase
