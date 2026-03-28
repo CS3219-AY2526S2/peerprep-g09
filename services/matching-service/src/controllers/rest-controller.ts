346845264029ce06f1ea4c5ff69aeab03cce55ca
Minh <ngohoangnhatminh@gmail.com>
2026-03-28 15:20:04 +0800

    Remove unused import

--- services/matching-service/src/controllers/rest-controller.ts
+++ services/matching-service/src/controllers/rest-controller.ts
@@ -1,4 +1,4 @@
-import { response, type Request, type Response } from "express";
+import { type Request, type Response } from "express";
 import axios from "axios";
 import redis from "../services/redisService.js";
 
